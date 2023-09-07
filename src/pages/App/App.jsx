191c4492b035d49b5b98c0410cd9804f27013b01
SamuelBai0910 <baisimo2022@163.com>
2023-09-07 22:10:54 +1000

    add Navigate to App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
@@ -19,6 +19,7 @@ export default function App() {
               {/* Route components in here */}
               <Route path="/orders/new" element={<NewOrderPage />} />
               <Route path="/orders" element={<OrderHistoryPage />} />
+              <Route path="*" element={<Navigate to="/orders" />} />
             </Routes>
           </>
           :
